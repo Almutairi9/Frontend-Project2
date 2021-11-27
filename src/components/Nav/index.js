63e4656434d8f956125b70e2d61aec014d1f3858
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-11-27 18:52:55 +0300

    still

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import "react-bootstrap";
-import { Link } from "react-router-dom";
-
 // import { useEffect } from "react";
 import "./style.css";
 
@@ -19,15 +17,24 @@ const Nav = () => {
     <div>
       <header className="header">
         <nav className="navbar">
-          <img src="/Newss.jpg" alt="News Logo" className="nav-logo"/>
-  
-          <div class="top-right">
-
-            <Link to="/Signup" class="btn">Sign up</Link>
-            <Link to="/Signup" class="btn">Sign up</Link>
-</div>
+          <img src="Newss.jpg" alt="News Logo" className="nav-logo"/>
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <button
+                class="btn btn-primary"
+                type="submit"
+                onClick={() => navigate(`/Profile`)}
+              >
+                my profile{" "}
+              </button>
               {/* <button class="btn btn-primary" onClick={() => navigate(`/Profile`)}>my profile </button> */}
-           
+            </li>
+            <li className="nav-item">
+              <button class="btn btn-primary" type="submit" onClick={logOut}>
+                Log out
+              </button>
+            </li>
+          </ul>
         </nav>
       </header>
     </div>
